6885919d01f3826534a2a74ca4b08c9e7e0d807c
fmzhen <1045561531@qq.com>
2025-12-16 11:22:58 +0800

    项目初始化并安装Chakra UI（V2版本）

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+// / 1. 导入Button组件
+import { Button, ButtonGroup } from "@chakra-ui/react";
+// 2. 渲染按钮
+function App() {
+  return <Button colorScheme="blue">Button</Button>;
+}
+export default App;
