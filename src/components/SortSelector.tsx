82b14769a41a03d0a592dd01a6eb9803fe4c1c8f
fmzhen <1045561531@qq.com>
2025-12-18 16:54:06 +0800

    对游戏排序

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,37 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-
-const SortSelector = () => {
+interface Props {
+  selectedSortOrder: string;
+  onSelectedSortOrder: (sortOrder: string) => void;
+}
+const SortSelector = ({ selectedSortOrder, onSelectedSortOrder }: Props) => {
+  // 定义排序数组
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date Added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === selectedSortOrder
+  );
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        order by: relevance
+        order by: {currentSortOrder?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>relevance</MenuItem>
-        <MenuItem>date added</MenuItem>
-        <MenuItem>name</MenuItem>
-        <MenuItem>release date</MenuItem>
-        <MenuItem>popularity</MenuItem>
-        <MenuItem>average rating</MenuItem>
+        {sortOrders.map((sortOrder) => (
+          <MenuItem
+            onClick={() => onSelectedSortOrder(sortOrder.value)}
+            key={sortOrder.value}
+            value={sortOrder.value}
+          >
+            {sortOrder.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -17,8 +17,10 @@ const useGames = (gameQuery: GameQuery) => useData<Game>(
   '/games',
   {params:{
     genres : gameQuery.genre?.id,
-    platforms: gameQuery.platform?.id}},
-  [gameQuery.genre?.id,gameQuery.platform?.id]
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
+  }},
+  [gameQuery]
 )
 
 export default useGames;
