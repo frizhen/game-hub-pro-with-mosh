1c87f6d5472e47296b8b721d3d380440f0ce0975
fmzhen <1045561531@qq.com>
2025-12-24 14:52:27 +0800

    重构数据获取逻辑

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-export interface Platform{
+import apiClient, { FetchResponse } from "../services/api-client";
+export interface Platform {
   id: number;
   name: string;
   slug: string;
@@ -9,21 +10,24 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform:Platform}[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
-  rating_top:number;
+  rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) => useData<Game>(
-  '/games',
-  {params:{
-    genres : gameQuery.genre?.id,
-    parent_platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText
-  }},
-  [gameQuery]
-)
-
+const useGames = (gameQuery: GameQuery) =>
+  useQuery({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 export default useGames;
-
