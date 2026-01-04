cd2d18ff2bd515f6fee602727787a9425b67dbf0
fmzhen <1045561531@qq.com>
2026-01-04 17:27:56 +0800

    详情页添加screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import useScreenshots from "../hooks/useScreenshots";
+import { Image, SimpleGrid, Spinner } from "@chakra-ui/react";
+interface Props {
+  gameId: number;
+}
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useScreenshots(gameId);
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map((screenshot) => (
+        <Image
+          src={screenshot.image}
+          key={screenshot.id}
+          alt={`Screenshot for game ${gameId}`}
+        />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
--- src/pages/GameDetialPage.tsx
+++ src/pages/GameDetialPage.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetialPage = () => {
   const { slug } = useParams();
@@ -14,6 +16,8 @@ const GameDetialPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
