165912640e80d364868b672119c2f9c78f07894c
fmzhen <1045561531@qq.com>
2026-01-04 17:53:14 +0800

    导航栏问题修复

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,20 @@ import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (inputRef.current) setSearchText(inputRef.current.value);
+        if (inputRef.current) {
+          setSearchText(inputRef.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup>
