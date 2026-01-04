ab687999ac7d2d11470ddbb13d21c779d40cc5b4
fmzhen <1045561531@qq.com>
2026-01-04 17:42:33 +0800

    游戏详情页响应式双列布局

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import useScreenshots from "../hooks/useScreenshots";
 import { Image, SimpleGrid, Spinner } from "@chakra-ui/react";
+import useScreenshots from "../hooks/useScreenshots";
 interface Props {
   gameId: number;
 }
