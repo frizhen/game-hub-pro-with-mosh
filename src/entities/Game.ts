06736203c1c4abeba7558718898573e3c9c09f4f
fmzhen <1045561531@qq.com>
2026-01-04 15:09:55 +0800

    为详情页添加游戏属性

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string; // 新增属性
   background_image: string;
   parent_platforms: { platform: Platform }[];
