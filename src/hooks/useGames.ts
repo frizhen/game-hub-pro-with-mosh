eb21a6dbec7d2ebfcb9f7631b67555e90a3d50ae
fmzhen <1045561531@qq.com>
2026-03-17 10:48:03 +0800

    请求竞态

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,30 +1,64 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
+// import { useInfiniteQuery } from "@tanstack/react-query";
 
+// import APIClient from "../services/api-client";
+// import ms from "ms";
+// import useGameQueryStore from "../store";
+// import Game from "./../entities/Game";
+
+// const apiClient = new APIClient<Game>("/games");
+// const useGames = () => {
+//   const gameQuery = useGameQueryStore((s) => s.gameQuery);
+//   return useInfiniteQuery({
+//     queryKey: ["games", gameQuery],
+//     queryFn: ({ pageParam = 1 }) =>
+//       apiClient.getAll({
+//         params: {
+//           genres: gameQuery.genreId,
+//           parent_platforms: gameQuery.platformId,
+//           ordering: gameQuery.sortOrder,
+//           search: gameQuery.searchText,
+//           page: pageParam,
+//         },
+//       }),
+//     getNextPageParam: (lastPage, allPages) => {
+//       return lastPage.next ? allPages.length + 1 : undefined;
+//     },
+//     initialPageParam: 1,
+//     staleTime: ms("24h"),
+//   });
+// };
+// export default useGames;
+import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import ms from "ms";
 import useGameQueryStore from "../store";
-import Game from "./../entities/Game";
+import Game from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
+
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
+
   return useInfiniteQuery({
     queryKey: ["games", gameQuery],
-    queryFn: ({ pageParam = 1 }) =>
-      apiClient.getAll({
-        params: {
-          genres: gameQuery.genreId,
-          parent_platforms: gameQuery.platformId,
-          ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText,
-          page: pageParam,
+    queryFn: ({ pageParam = 1, signal }) =>
+      apiClient.getAll(
+        {
+          params: {
+            genres: gameQuery.genreId,
+            parent_platforms: gameQuery.platformId,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+            page: pageParam,
+          },
         },
-      }),
-    getNextPageParam: (lastPage, allPages) => {
-      return lastPage.next ? allPages.length + 1 : undefined;
-    },
+        signal,
+      ),
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.next ? allPages.length + 1 : undefined,
     initialPageParam: 1,
     staleTime: ms("24h"),
   });
 };
+
 export default useGames;
