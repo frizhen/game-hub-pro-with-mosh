14d22faa2f4c0e98835cae3bd485b5246c6480e1
fmzhen <1045561531@qq.com>
2026-04-03 10:11:09 +0800

    security: re-add api-client after purging leaked history

---
+++ src/services/api-client.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_RAWG_API_KEY,
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig = {}, signal?: AbortSignal) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, { ...config, signal })
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string, signal?: AbortSignal) => {
+    return axiosInstance
+      .get<T>(`${this.endpoint}/${id}`, { signal })
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
